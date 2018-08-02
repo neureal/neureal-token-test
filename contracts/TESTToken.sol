ecb386c92124f20df1f61418601fb234d3a5162d
Wil Bown <wilbown@gmail.com>
2018-08-01 19:12:40 -0600

    mythril was throwing a reachable exception
    I wanted to keep these as exceptions so that anyone trying them would have to pay the maximum gas lol But in the interest of community understanding I changed them to require. I could possibly do require pre-checks in the main functions, but that is less efficient.

--- contracts/TESTToken.sol
+++ contracts/TESTToken.sol
@@ -4,19 +4,19 @@ library SafeMath {
     function mul(uint256 a, uint256 b) internal pure returns (uint256 c) {
         if (a == 0) return 0;
         c = a * b;
-        assert(c / a == b);
+        require(c / a == b);
         return c;
     }
     function div(uint256 a, uint256 b) internal pure returns (uint256) {
         return a / b;
     }
     function sub(uint256 a, uint256 b) internal pure returns (uint256) {
-        assert(b <= a);
+        require(b <= a);
         return a - b;
     }
     function add(uint256 a, uint256 b) internal pure returns (uint256 c) {
         c = a + b;
-        assert(c >= a);
+        require(c >= a);
         return c;
     }
 }
