ef30f6938062c715cdb98c8c8cde86812a1dc1c6
Wil Bown <wilbown@gmail.com>
2018-08-15 19:28:59 -0600

    update SafeMath per OpenZeppelin. UTF8 duh

--- contracts/TESTToken.sol
+++ contracts/TESTToken.sol
@@ -1,22 +1,24 @@
-pragma solidity 0.4.24;
+﻿pragma solidity 0.4.24;
 
-// TODO change these back to assert and add require pre-checks in functions that use them
 library SafeMath {
-    function mul(uint256 a, uint256 b) internal pure returns (uint256 c) {
+    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
         if (a == 0) return 0;
-        c = a * b;
+        uint256 c = a * b;
         require(c / a == b);
         return c;
     }
     function div(uint256 a, uint256 b) internal pure returns (uint256) {
-        return a / b;
+        require(b > 0);
+        uint256 c = a / b;
+        return c;
     }
     function sub(uint256 a, uint256 b) internal pure returns (uint256) {
         require(b <= a);
-        return a - b;
+        uint256 c = a - b;
+        return c;
     }
-    function add(uint256 a, uint256 b) internal pure returns (uint256 c) {
-        c = a + b;
+    function add(uint256 a, uint256 b) internal pure returns (uint256) {
+        uint256 c = a + b;
         require(c >= a);
         return c;
     }
